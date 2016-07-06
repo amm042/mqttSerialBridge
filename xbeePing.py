6dffeaefbbd600390fc10d09872d5f26592372bf
alan <alan@raspberrypi>
2016-07-06 11:37:12 -0400

    adding pongs

--- xbeePing.py
+++ xbeePing.py
@@ -1,17 +1,25 @@
 import xbeeDevice
 import time
+import logging
+#$logging.basicConfig(level=logging.DEBUG)
 
-
-def rx(data):
-    print("RX: ", data)
+def rx(dev, src, data):
     
+    print("RX: [{:x}, {}dBm]: {}".format(src, dev.rssi_history[-1], data))
+    
+    xb.send(b'PONG', dest = src)
+    print("TX [{:x}]: PONG ".format(src))
 xb = xbeeDevice.XBeeDevice('/dev/ttyUSB0:38400:8N1', rx)
 
 try:
     while True:
-        print("TX: PING")
+        print("TX [{:x}]: PING".format(0xffff))
         xb.send(b'PING', dest = 0xffff)
         time.sleep(0.5)
     
 finally:
     xb.close()    
+    
+    
+    
+    
