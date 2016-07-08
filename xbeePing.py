d5b930f5b2d7848091e15297935108554c9efc75
Alan <amm042@bucknell.edu>
2016-07-08 12:16:49 -0400

    file log

--- xbeePing.py
+++ xbeePing.py
@@ -2,14 +2,24 @@ import xbeeDevice
 import time
 import struct
 import logging
-logging.basicConfig(level=logging.INFO)
+import random
+#logging.basicConfig(level=logging.INFO)
+
+logfile =  os.path.splitext(sys.argv[0])[0] + ".log"
+
+logging.basicConfig(level=logging.INFO,
+                    handlers=(logging.StreamHandler(stream=sys.stdout),
+                              logging.handlers.RotatingFileHandler(logfile,
+                                                                    maxBytes = 256*1024,
+                                                                    backupCount = 6), ),
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def rx(dev, src, data):
     
-    print("RX [{:x}, {}dBm]: {}".format(src, dev.rssi_history[-1], data))
+    logging.info("RX [{:x}, {}dBm]: {}".format(src, dev.rssi_history[-1], data))
     
     xb.send(b'PONG', dest = src)
-    print("TX [{:x}]: PONG ".format(src))
+    logging.info("TX [{:x}]: PONG ".format(src))
 xb = xbeeDevice.XBeeDevice('/dev/ttyUSB0:38400:8N1', rx)
 
 try:
@@ -18,17 +28,17 @@ try:
     xb.send_cmd("at", command=b'ID', parameter = b'\x33\x33')
     
     # channel mask for dana roof from scanning
-    xb.send_cmd("at", command=b'CM', parameter=struct.pack(">Q", 0xffffffc000000000))
+    xb.send_cmd("at", command=b'CM', parameter=struct.pack(">Q", 0xfceb29f032404210))
     xb.send_cmd("at", command=b'CM')
     time.sleep(0.5)
     xb.send_cmd("at", command=b'ED')
     while True:
-        print("TX [{:x}]: PING".format(0xffff))
+        logging.info("TX [{:x}]: PING".format(0xffff))
         xb.send(b'PING', dest = 0xffff)
         
         
         
-        time.sleep(0.5)
+        time.sleep(0.5 + random.random())
         
         
         
